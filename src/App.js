c4a308363e7dc2b071c0110fb9fc130fbea71e8c
omarhernandezrey <hernandezreyomar@gmail.com>
2023-10-05 13:40:55 -0500

    03 Componentes de TODO Machine

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import {TodoCounter} from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import './App.css'; 
+
+function App() {
+  return (
+    <div className="App">
+
+      <TodoCounter />
+      <TodoSearch />
+
+    <TodoList>
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+    </TodoList>
+
+    {/* <CeateTodoButton /> */}
+    </div>
+  );
+}
+
+export default App;
