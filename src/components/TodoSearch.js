99c670d2b2d398f16756dc581de80ff487b3ada2
omarhernandezrey <hernandezreyomar@gmail.com>
2023-10-09 17:24:15 -0500

    07 ¿Qué es el estado?

--- src/components/TodoSearch.js
+++ src/components/TodoSearch.js
@@ -1,16 +1,17 @@
+import React from 'react';
 import './TodoSearch.css'
 function TodoSearch() {
+    const [searchValue, setSearchValue] = React.useState('');
+
+    console.log('Los usuarios buscan Todos de ' + searchValue);
+
     return (
       <input 
       placeholder="Cortar cebolla"  
       className='TodoSearch'
+      value={searchValue} 
       onChange={(event) => {
-        console.log('evento del teclado en el imput TodoSearch');
-        console.log(event);
-        console.log(event.target);
-        console.log(event.target.value);
-
-
+        setSearchValue(event.target.value);
       }}
       />
     );
