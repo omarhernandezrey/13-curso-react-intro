1b0198294f7767d375a7763b5e8d256597730d01
OmarHernandezRey <hernandezreyomar@gmail.com>
2023-11-03 16:33:00 -0500

    22 ¿Qué es React Context?

--- src/TodoContext/useLocalStorage.js
+++ src/TodoContext/useLocalStorage.js
@@ -4,39 +4,54 @@ function useLocalStorage(itemName, initialValue) {
   const [item, setItem] = React.useState(initialValue);
   const [loading, setLoading] = React.useState(true);
   const [error, setError] = React.useState(false);
-
+  
   React.useEffect(() => {
     setTimeout(() => {
       try {
         const localStorageItem = localStorage.getItem(itemName);
+    
         let parsedItem;
-
+  
         if (!localStorageItem) {
           localStorage.setItem(itemName, JSON.stringify(initialValue));
           parsedItem = initialValue;
         } else {
           parsedItem = JSON.parse(localStorageItem);
-          // Sort the items here (assuming the item is an array)
-          parsedItem.sort();
           setItem(parsedItem);
         }
-
+  
         setLoading(false);
-      } catch (error) {
+      } catch(error) {
         setLoading(false);
         setError(true);
       }
     }, 2000);
-  }, []);
+  });
 
   const saveItem = (newItem) => {
-    // If your item is an array, you can sort it before saving
-    newItem.sort();
     localStorage.setItem(itemName, JSON.stringify(newItem));
     setItem(newItem);
   };
 
-  return { item, saveItem, loading, error };
+  return {
+    item,
+    saveItem,
+    loading,
+    error,
+  };
 }
 
 export { useLocalStorage };
+
+
+// localStorage.removeItem('TODOS_V1');
+
+// const defaultTodos = [
+//   { text: 'Cortar cebolla', completed: true },
+//   { text: 'Tomar el Curso de Intro a React.js', completed: false },
+//   { text: 'Llorar con la Llorona', completed: false },
+//   { text: 'LALALALALA', completed: false },
+//   { text: 'Usar estados derivados', completed: true },
+// ];
+
+// localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
